7b5baf799d549a5ff3a88349d50161a62211f7e1
klar <a@pxd.ch>
2019-03-26 22:12:02 +0100

    do not show log output

--- js/global.js
+++ js/global.js
@@ -1,6 +1,6 @@
 function generateQr(event, elId) {
   // console.log(event.target.value);
-  console.log(elId);
+  // console.log(elId);
   var qr = new QRious({
     element: document.getElementById(elId),
     size: 150,
